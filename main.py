bc20cbd34657734cbe4e5510934f84b28ec1a2e0
Ali Bencheikh <alibencheikh212@gmail.com>
2024-02-02 14:55:01 +0100

    update and changed some comments

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-# import operetting system
 import os
 pygame.font.init()
 pygame.mixer.init()
@@ -7,7 +6,7 @@ pygame.mixer.init()
 # la resulotion de la fenetre pygame
 WIGHT, HEIGHT = 1000, 600
 WIN = pygame.display.set_mode((WIGHT, HEIGHT))
-pygame.display.set_caption("ALi second game")
+pygame.display.set_caption("SPACE WAR")
 
 # les couleur et la bordure
 WHITE = (255, 255, 255)
@@ -16,14 +15,15 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 BORDER = pygame.Rect(WIGHT//2 - 5, 0, 10, HEIGHT)
 
- #le son de tire
+ #the games sounds
 BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join(
-    'Assets', 'get-hit.mp3'))
+    'assets', 'get-hit.mp3'))
 BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join(
-    'Assets', 'tir-al.ogg'))
+    'assets', 'hit.ogg'))
 GAME_OVER = pygame.mixer.Sound(os.path.join(
-    'Assets', 'game_over.ogg'))
-# Le text de vinceur
+    'assets', 'game_over.ogg'))
+
+# the winners text and the health text
 HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
 WINNER_FONT = pygame.font.SysFont('comicsans', 100)
 
@@ -35,31 +35,30 @@ SPACESHIP_WIGHT, SPACESHIP_HEIGHT = 55, 40
 YELLOW_HIT = pygame.USEREVENT + 1
 RED_HIT = pygame.USEREVENT + 2
 
-# IMPORTER LES IMAGE
-# le premier joueur
+# the first player
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
-    os.path.join('Assets', 'rocket.png'))
+    os.path.join('assets', 'rocket.png'))
 YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
     YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIGHT, SPACESHIP_HEIGHT)), 270)
 
-# le duexieme joueur
+# the seceond player
 RED_SPACESHIP_IMAGE = pygame.image.load(
-    os.path.join('Assets', 'spaceship.png'))
+    os.path.join('assets', 'spaceship.png'))
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
     RED_SPACESHIP_IMAGE, (SPACESHIP_WIGHT, SPACESHIP_HEIGHT)), 90)
 
-# le background
+# the background
 SPACE = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'space.jpg')), (WIGHT, HEIGHT))
+    os.path.join('assets', 'space.jpg')), (WIGHT, HEIGHT))
 
 def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
     WIN.blit(SPACE, (0, 0))
     pygame.draw.rect(WIN, BLACK, BORDER)
 
     red_health_text = HEALTH_FONT.render(
-        "Vie de blue: " + str(red_health), 1, WHITE)
+        "Blue's helth: " + str(red_health), 1, WHITE)
     yellow_health_text = HEALTH_FONT.render(
-        "Vie de rouge: " + str(yellow_health), 1, WHITE)
+        "Red's helth: " + str(yellow_health), 1, WHITE)
     WIN.blit(red_health_text, (WIGHT - red_health_text.get_width() - 10, 10))
     WIN.blit(yellow_health_text, (10, 10))
 
@@ -75,23 +74,23 @@ def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_hea
 
 
 def yellow_handle_movment(keys_pressed, yellow):
-    if keys_pressed[pygame.K_q] and yellow.x - VEL > 0:  # a gauche
+    if keys_pressed[pygame.K_q] and yellow.x - VEL > 0:
         yellow.x -= VEL
-    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:  # a droite
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:
         yellow.x += VEL
-    if keys_pressed[pygame.K_z] and yellow.y -VEL > 0:  # en haut
+    if keys_pressed[pygame.K_z] and yellow.y -VEL > 0:
         yellow.y -= VEL
-    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 10:  # en bas
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 10:
         yellow.y += VEL
 
 def red_handle_movment(keys_pressed, red):
-    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:  # a gauche
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:  
         red.x -= VEL
-    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIGHT:  # a droite
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIGHT:
         red.x += VEL
-    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:  # en haut
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:  
         red.y -= VEL
-    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 10:  # en bas
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 10:
         red.y += VEL
 
 
@@ -141,13 +140,13 @@ def main():
                 pygame.quit()
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                if event.key == pygame.K_e and len(yellow_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(
                         yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
                     yellow_bullets.append(bullet)
                     BULLET_FIRE_SOUND.play()
 
-                if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                if event.key == pygame.K_m and len(red_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(
                         red.x, red.y + red.height//2 - 2, 10, 5)
                     red_bullets.append(bullet)
@@ -163,11 +162,11 @@ def main():
 
         winner_text = ""
         if red_health <= 0:
-            winner_text = "Le rouge as Gagner!!"
+            winner_text = "Winner : RED!!"
             GAME_OVER.play()
 
         if yellow_health <= 0:
-            winner_text = "Le blue as Gagner!!"
+            winner_text = "Winner : BLUE!!"
             GAME_OVER.play()
 
         if winner_text != "":
